80ec2eff1fb2ba97d8abe5a877752d1582f74ba9
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-03-12 00:31:30 +0700

    belajar dart packages

---
+++ bin/belajar_dart_application.dart
@@ -0,0 +1,10 @@
+
+import 'package:belajar_dart_library/hello.dart' as belajar;
+
+void main(){
+  print(belajar.sayHello("Eko"));
+
+  var customer = belajar.Customer();
+  var product = belajar.Product();
+  var category = belajar.Category();
+}
---
+++ test/belajar_dart_application_test.dart
@@ -0,0 +1,8 @@
+import 'package:belajar_dart_application/belajar_dart_application.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
